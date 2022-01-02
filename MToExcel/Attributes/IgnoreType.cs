292c58fa7a61dda381bc946de1bc1c1bc4657d07
黄金精神 <1537004059@qq.com>
2022-01-02 20:25:20 +0800

    增加Ignore注解

---
+++ MToExcel/Attributes/IgnoreType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MToExcel.Attributes
+{
+    /// <summary>
+    /// 忽略制定标记的属性值
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false,Inherited =true)]
+    public class IgnoreType:Attribute
+    {
+
+        public IgnoreType()
+        {
+
+        }
+    }
+}
