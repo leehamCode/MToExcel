5590e92cf3f08bd3154e6b289116504e3e570020
leehan <1537004059@qq.com>
2024-01-19 13:44:07 +0800

    add表达式解析工具

---
+++ MToExcel/Utils/ExpressionHelp.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MToExcel.Utils
+{
+    public class ExpressionHelp
+    {
+        /// <summary>
+        /// 读取标题内容
+        /// 只读取第一个对象
+        /// </summary>
+        /// <param name="exp"></param>
+        /// <returns></returns>
+        public string Read_Title_Content(string exp)
+        {
+
+            return "";
+        }
+
+        /// <summary>
+        /// 读取条件隐藏的信息
+        /// 
+        /// </summary>
+        /// <param name="exp"></param>
+        /// <returns></returns>
+        public bool Read_Condition_expression(string exp)
+        {
+            return true;
+        }
+    }
+}
